0a78fc81198ba6e70a52dbe87fe9e23c04d78dec
Ash <smashraful1995@gmail.com>
2022-06-20 17:46:56 +0600

    3rd commit

--- assets/application.js
+++ assets/application.js
@@ -8,3 +8,33 @@ if (document.getElementById("sort_by") != null) {
     window.location = url.href;
   });
 }
+if (document.getElementById("AddressCountryNew") != null) {
+  document
+    .getElementById("AddressCountryNew")
+    .addEventListener("change", function (e) {
+      var provinces =
+        this.options[this.selectedIndex].getAttribute("data-provinces");
+      var provinceSelector = document.getElementById("AddressProvinceNew");
+      var provinceArray = JSON.parse(provinces);
+
+      //console.log(provinceArray);
+      if (provinceArray.length < 1) {
+        provinceSelector.setAttribute("disabled", "disabled");
+      } else {
+        provinceSelector.removeAttribute("disabled");
+      }
+
+      provinceSelector.innerHTML = "";
+      var options = "";
+      for (var i = 0; i < provinceArray.length; i++) {
+        options +=
+          '<option value="' +
+          provinceArray[i][0] +
+          '">' +
+          provinceArray[i][0] +
+          "</option>";
+      }
+
+      provinceSelector.innerHTML = options;
+    });
+}
